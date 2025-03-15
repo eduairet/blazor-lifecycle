1620723b5eb876ebb5af850d948ce9c3b1245910
eduairet <hola@eduairet.com>
2025-03-14 21:26:04 -0600

    Setup with tailwind

---
+++ BlazorLifecycle/BlazorLifecycle.Client/Shared/Constants/PageUrls.cs
@@ -0,0 +1,9 @@
+namespace BlazorLifecycle.Client.Shared.Constants;
+
+public class PageUrls
+{
+    public const string Home = "/";
+    public const string Error = "/error";
+    public const string SetParametersAsync = "/set-parameters-async";
+    public const string Counter = "/counter";
+}

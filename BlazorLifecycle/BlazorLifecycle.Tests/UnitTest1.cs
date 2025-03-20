b50478a558c1f531aeacc486c14957ffa05afe8e
eduairet <hola@eduairet.com>
2025-03-19 22:07:04 -0600

    Test init

---
+++ BlazorLifecycle/BlazorLifecycle.Tests/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.RegularExpressions;
+using Microsoft.Playwright;
+using Microsoft.Playwright.Xunit;
+
+namespace BlazorLifecycle.Tests;
+
+public class UnitTest1 : PageTest
+{
+    [Fact]
+    public async Task HasTitle()
+    {
+        await Page.GotoAsync("https://playwright.dev");
+
+        // Expect a title "to contain" a substring.
+        await Expect(Page).ToHaveTitleAsync(new Regex("Playwright"));
+    }
+
+    [Fact]
+    public async Task GetStartedLink()
+    {
+        await Page.GotoAsync("https://playwright.dev");
+
+        // Click the get started link.
+        await Page.GetByRole(AriaRole.Link, new PageGetByRoleOptions { Name = "Get started" }).ClickAsync();
+
+        // Expects page to have a heading with the name of Installation.
+        await Expect(Page.GetByRole(AriaRole.Heading, new PageGetByRoleOptions { Name = "Installation" }))
+            .ToBeVisibleAsync();
+    }
+}
